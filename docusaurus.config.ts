416691ef71250e790631725b8c5e4fe948021c3f
Phoax <factorimio@gmail.com>
2024-01-29 23:34:42 +0100

    feat: update doc page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -113,7 +113,7 @@ const config: Config = {
             },
             {
               label: "Twitter",
-              href: "https://twitter.com/docusaurus",
+              href: "https://twitter.com/paranodeio",
             },
           ],
         },
